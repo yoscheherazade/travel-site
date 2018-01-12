e7e38b99784d26bce31b80862ae20c0a55c53191
LaQuita <lgollman@gmail.com>
2018-01-11 20:16:31 -0800

    added browsersync, modularized css

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp          = require('gulp'),
     autoprefixer  = require('autoprefixer'),
     cssvars       = require('postcss-simple-vars'),
     nested        = require('postcss-nested'),
-    cssImport     = require('postcss-import');
+    cssImport     = require('postcss-import'),
+    browserSync   = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task!");
@@ -21,6 +22,12 @@ gulp.task('styles',function() {
 });
 
 gulp.task('watch', function() {
+
+  broswerSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
 
   watch('./app/index.html', function() {
     gulp.start('html');
